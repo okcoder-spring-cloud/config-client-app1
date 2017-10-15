d5adfcab5091a326d57a7db2b84632cecd2760a1
okcoder <okcoder@live.com>
2017-10-15 10:50:30 +0900

    create project from https://start.spring.io/  Selected Dependencies   Config Client   Web

---
+++ src/main/java/org/okcoder/springcloud/configclientapp1/ConfigClientApp1Application.java
@@ -0,0 +1,12 @@
+package org.okcoder.springcloud.configclientapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApp1Application.class, args);
+	}
+}
---
+++ src/test/java/org/okcoder/springcloud/configclientapp1/ConfigClientApp1ApplicationTests.java
@@ -0,0 +1,16 @@
+package org.okcoder.springcloud.configclientapp1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigClientApp1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
